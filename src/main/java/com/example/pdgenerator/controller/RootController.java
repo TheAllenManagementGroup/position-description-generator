ea0783bfe0ceb90fd8893e44838cd4729bd19e6f
Marko Vukovic <marko.vukovic0311@gmail.com>
2025-09-19 10:46:01 -0500

    Add files via upload

---
+++ src/main/java/com/example/pdgenerator/controller/RootController.java
@@ -0,0 +1,13 @@
+package com.example.pdgenerator.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/")
+    public String root() {
+        return "PD Generator is running! Use /api/generate to POST requests.";
+    }
+}
